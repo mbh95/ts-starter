2c2d969d9e3dad9f89af4be58bfe574130f6c9a0
mbh95 <matthewbh95@gmail.com>
2021-04-15 21:25:58 -0700

    Update ESLint config.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+{
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "extends": [
+    "recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "rules": {
+  },
+  "env": {
+    "browser": true,
+    "amd": true,
+    "node": true
+  }
+}
